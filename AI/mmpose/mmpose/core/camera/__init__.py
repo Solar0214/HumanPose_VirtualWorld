871f4217a9c64e059e9dda470ed00c657fc49b1d
nikstery55 <nikstery55@gmail.com>
2021-11-23 15:14:45 +0900

    필요없는모델제거
    필요없는모델제거

--- AI/mmpose/mmpose/core/camera/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .camera_base import CAMERAS
-from .single_camera import SimpleCamera
-
-__all__ = ['CAMERAS', 'SimpleCamera']
--- AI/mmpose/mmpose/models/necks/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .gap_neck import GlobalAveragePooling
-
-__all__ = ['GlobalAveragePooling']
