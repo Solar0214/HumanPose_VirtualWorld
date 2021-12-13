4b2fc6d9d0332961942bbd0215256ae418e19bfe
nikstery55 <nikstery55@gmail.com>
2021-12-13 09:43:23 +0900

    필요없는코드

--- AI/Communication_protocol/python_UDP_receiver.py
+++
@@ -1,9 +0,0 @@
-import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    # 소켓을 생성한다
-sock.bind(("127.0.0.1", 8080))
-while True:
-    data, addr = sock.recvfrom(1024)
-    data = data.decode().upper()
-    sock.sendto(data.encode(), addr)
-    print(data, addr)
-sock.close()
